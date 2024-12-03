d799fe073d6cf9f8e5d10cb99afdc0a983c2de82
HaiwangYu <yuhw.pku@gmail.com>
2024-12-03 11:09:24 -0600

    update

--- setup.sh
+++ setup.sh
@@ -1,10 +1,32 @@
 source /exp/sbnd/app/users/yuhw/setup.sh
 
+path-remove ()
+{
+    local IFS=':';
+    local NEWPATH;
+    local DIR;
+    local PATHVARIABLE=${2:-PATH};
+    for DIR in ${!PATHVARIABLE};
+    do
+        if [ "$DIR" != "$1" ]; then 
+            NEWPATH=${NEWPATH:+$NEWPATH:}$DIR;
+        fi;
+    done;   
+    export $PATHVARIABLE="$NEWPATH"
+}
+
 path-prepend ()
 {
     path-remove "$1" "$2";
     local PATHVARIABLE="${2:-PATH}";
     export $PATHVARIABLE="$1${!PATHVARIABLE:+:${!PATHVARIABLE}}"
+}
+
+path-append ()
+{
+    path-remove "$1" "$2";
+    local PATHVARIABLE="${2:-PATH}";
+    export $PATHVARIABLE="${!PATHVARIABLE:+${!PATHVARIABLE}:}$1"
 }
 
 workdir=/exp/sbnd/app/users/yuhw/dnn-roi/
