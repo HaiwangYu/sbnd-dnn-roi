c0aa9fba2ce993fb3fc79fbbcfed72e2058224af
HaiwangYu <yuhw.pku@gmail.com>
2024-12-03 10:57:37 -0600

    define path-prepend in place

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 source /exp/sbnd/app/users/yuhw/setup.sh
 
+path-prepend ()
+{
+    path-remove "$1" "$2";
+    local PATHVARIABLE="${2:-PATH}";
+    export $PATHVARIABLE="$1${!PATHVARIABLE:+:${!PATHVARIABLE}}"
+}
+
 path-prepend `pwd`/moon/wire-cell-data WIRECELL_PATH
 path-prepend /exp/sbnd/app/users/yuhw/wire-cell-toolkit/cfg WIRECELL_PATH
 path-prepend `pwd`/moon/wire-cell-toolkit/cfg/ WIRECELL_PATH
